1bed0b883fcb71a9f620d2bd5f58a31ecc8e4d71
kelvingamedev <kelvin.gamedeveloper@gmail.com>
2016-02-12 01:27:08 -0200

    Commit inicial

---
+++ Assets/Scripts/TheGameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TheGameManager : MonoBehaviour {
+
+    /// <summary>
+    /// Reference to the Text Componenet to the number to be round
+    /// </summary>
+    public Text NumToBeRound; 
+
+	// Use this for initialization
+	void Start () {
+        double num =  Random.Range(-9999.9999f, -9999.9999f);
+        NumToBeRound.text = num.ToString(); 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
